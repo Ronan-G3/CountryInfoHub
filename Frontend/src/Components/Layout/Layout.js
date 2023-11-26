3e3ce150e067a7b840a90148fa584a224330dd58
Ronan-G3 <123460617+Ronan-G3@users.noreply.github.com>
2023-11-26 17:32:49 +0000

    Updated styles with logos and banner, colour scheme

--- Frontend/src/Components/Layout/Layout.js
+++ Frontend/src/Components/Layout/Layout.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import styles from './Layout.module.css'; 
+import styles from './Layout.module.css';
 
 const Layout = ({ children }) => {
   return (
     <div className={styles.layout}>
-      <header className={styles.header}>Country Info Hub App</header>
+      <header className={styles.header}>
+        <img src={`${process.env.PUBLIC_URL}/Country_Info_Banner2.png`} alt="Country Info Banner" className={styles.banner} />
+        </header>
+      <div className={styles.logoContainer}>
+        <img src={`${process.env.PUBLIC_URL}/Country_Info.png`} alt="Country Info Hub Logo" className={styles.logo} />
+      </div>
       <main className={styles.mainContent}>
         {children}
       </main>
