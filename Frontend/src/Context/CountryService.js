9909432393d57b06ad4087be725d29784cdf309b
Ronan-G3 <123460617+Ronan-G3@users.noreply.github.com>
2023-11-26 15:42:48 +0000

    Updated CSS

--- Frontend/src/Context/CountryService.js
+++ Frontend/src/Context/CountryService.js
@@ -1,10 +1,10 @@
-const BASE_URL = 'http://localhost:3001/api'; // Adjust if your backend is on a different port
+const BASE_URL = 'http://localhost:3001/api'; 
 
 
 // Function to fetch country data from the backend
 const fetchCountryData = async (countryName) => {
   try {
-    // Encode the country name to ensure the URL is properly formatted
+    // Encodes the country name to ensure the URL is properly formatted
     const response = await fetch(`${BASE_URL}/countries/${encodeURIComponent(countryName)}`);
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
@@ -13,7 +13,7 @@ const fetchCountryData = async (countryName) => {
     return data; // This should be the data returned from the REST Countries API
   } catch (error) {
     console.error("Could not fetch country data:", error);
-    // Handle errors as you see fit for your application
+    // Handle errors
     throw error;
   }
 };
