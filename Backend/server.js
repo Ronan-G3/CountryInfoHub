159da31d08ba41d216d65ffaeb4fad3de1ca3906
Ronan-G3 <123460617+Ronan-G3@users.noreply.github.com>
2023-11-27 22:07:01 +0000

    Updated server logic for backend deployment

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const cors = require('cors'); // Make sure cors is installed: npm install cors
+const cors = require('cors');
 const axios = require('axios');
+const path = require('path');
 
 // Initialize the Express app
 const app = express();
@@ -8,7 +9,7 @@ const app = express();
 // Apply cors middleware
 app.use(cors());
 
-// Define routes
+// Define routes for the API
 app.get('/api/countries/:countryName', async (req, res) => {
   try {
     const response = await axios.get(`https://restcountries.com/v3.1/name/${req.params.countryName}`);
@@ -19,7 +20,14 @@ app.get('/api/countries/:countryName', async (req, res) => {
   }
 });
 
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '../Frontend/build')));
 
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../Frontend/build/index.html'));
+});
 
 // Start the server
 const port = process.env.PORT || 3001;
