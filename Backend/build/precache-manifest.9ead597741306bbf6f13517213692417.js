b9723bd84fe011fa29c908e78a18dbcb3b8630d4
Ronan-G3 <123460617+Ronan-G3@users.noreply.github.com>
2023-11-27 22:52:13 +0000

    Updated server logic for backend deployment

---
+++ Backend/build/precache-manifest.9ead597741306bbf6f13517213692417.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd5752062b8436440a90a2bf7bf710b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "17c09232f65cc5e9bbcf",
+    "url": "/static/css/main.5af60daa.chunk.css"
+  },
+  {
+    "revision": "383e40c20d8a362572bb",
+    "url": "/static/js/2.efc8a7b8.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/static/js/2.efc8a7b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bde69dda5b65595e5be",
+    "url": "/static/js/3.f70fe28e.chunk.js"
+  },
+  {
+    "revision": "17c09232f65cc5e9bbcf",
+    "url": "/static/js/main.110f55e3.chunk.js"
+  },
+  {
+    "revision": "62de47845980ba5b466b",
+    "url": "/static/js/runtime-main.77c1390c.js"
+  }
+]);
