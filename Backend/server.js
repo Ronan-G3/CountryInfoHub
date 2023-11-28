2a318a14a6143bb17ec440368df43750d0ced1aa
Ronan-G3 <123460617+Ronan-G3@users.noreply.github.com>
2023-11-28 21:55:36 +0000

    updated server logic

--- Backend/server.js
+++ Backend/server.js
@@ -1,26 +1,18 @@
 const express = require('express');
-const cors = require('cors');
 const axios = require('axios');
 
-// Initialize the Express app
 const app = express();
+const port = process.env.PORT || 3001;
 
-// Apply cors middleware
-app.use(cors());
-
-// Define routes for the API
 app.get('/api/countries/:countryName', async (req, res) => {
   try {
     const response = await axios.get(`https://restcountries.com/v3.1/name/${req.params.countryName}`);
     res.json(response.data);
   } catch (error) {
-    console.error("Error fetching country data: ", error);
-    res.status(500).send(`Error fetching country data: ${error.message}`);
+    res.status(500).send('Error fetching country data');
   }
 });
 
-// Start the server
-const port = process.env.PORT || 3001;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
