1dc3373d08a210d826f4d1210431b8ba25b02c0f
Ronan-G3 <123460617+Ronan-G3@users.noreply.github.com>
2023-11-28 23:04:29 +0000

    updated server.js

--- Backend/server.js
+++ Backend/server.js
@@ -4,11 +4,14 @@ const axios = require('axios');
 const app = express();
 const port = process.env.PORT || 3001;
 
+app.use(express.json());
+
 app.get('/api/countries/:countryName', async (req, res) => {
   try {
     const response = await axios.get(`https://restcountries.com/v3.1/name/${req.params.countryName}`);
     res.json(response.data);
   } catch (error) {
+    console.error("Error fetching country data: ", error);
     res.status(500).send('Error fetching country data');
   }
 });
